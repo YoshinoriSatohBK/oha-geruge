0dfd256721aad6e1a3457e228a4bc6e6d839a555
yoshinori satoh <yoshinori.satoh.tokyo@gmail.com>
2018-12-16 23:58:01 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@ export default {
   data() {
     return {
       text:
-        'おはゲルゲ！\r\nおはゲルゲボタンで、あなたも簡単におはゲルゲ！\n\rhttps://musing-torvalds-d26081.netlify.com/\r#ディアブルボア'
+        'おはゲルゲ！\r\rおはゲルゲボタンで、あなたも簡単におはゲルゲ！\rhttps://musing-torvalds-d26081.netlify.com/\r#ディアブルボア'
     }
   },
   methods: {
