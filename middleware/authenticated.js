82a1d34031468632b4a3e08268e5df8b361c17ae
yoshinori satoh <yoshinori.satoh.tokyo@gmail.com>
2018-12-16 18:33:00 +0900

    update

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,7 +2,8 @@ import { API } from 'aws-amplify'
 
 export default async function({ store, redirect }) {
   // ユーザーが認証されていないとき
-  if (!localStorage.getItem('accessToken')) {
+  console.log()
+  if (!localStorage.getItem('access_token')) {
     const res = await API.post('backend', '/request_token', {})
     localStorage.setItem('oauth_token', res.oauth_token)
     localStorage.setItem('oauth_token_secret', res.oauth_token_secret)
