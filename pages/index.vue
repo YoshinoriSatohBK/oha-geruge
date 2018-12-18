b2c1f553e197c406ec9f8e178d9fa1dd3725f1aa
yoshinori satoh <yoshinori_satoh@insight-tech.co.jp>
2018-12-18 16:41:23 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@ export default {
     return {
       processing: false,
       text:
-        'おはゲルゲ！\r\n#ディアブルボア\r\n\r\nおはゲルゲボタンで気軽におはゲルゲ！\r\nhttps://musing-torvalds-d26081.netlify.com/'
+        'おはゲルゲ！\r\n#ディアブルボア\r\n\r\nおはゲルゲボタンで気軽におはゲルゲ！\r\nhttps://oha-geruge.netlify.com/'
     }
   },
   methods: {
